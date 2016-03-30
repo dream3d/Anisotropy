a772db69aea3d29ac4511b172b6f971831741286
OndrejSedivy <ondrejsedivy@post.cz>
2016-03-30 16:09:16 +0200

    Added files via upload

---
+++ AnisotropyConstants.h
@@ -0,0 +1,12 @@
+#ifndef _AnisotropyConstants_H_
+#define _AnisotropyConstants_H_
+
+#include <QtCore/QString>
+
+namespace AnisotropyConstants
+{
+  const QString AnisotropyPluginFile("AnisotropyPlugin");
+  const QString AnisotropyPluginDisplayName("Anisotropy");
+  const QString AnisotropyBaseName("Anisotropy");
+}
+#endif
