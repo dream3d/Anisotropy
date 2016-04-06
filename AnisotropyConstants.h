5e2e6eb8647c45e700ab74b09d119376d16a4ffa
Ondrej Sedivy <ondrejsedivy@post.cz>
2016-04-06 14:40:31 +0200

    Update AnisotropyConstants.h

--- AnisotropyConstants.h
+++ AnisotropyConstants.h
@@ -8,5 +8,13 @@ namespace AnisotropyConstants
   const QString AnisotropyPluginFile("AnisotropyPlugin");
   const QString AnisotropyPluginDisplayName("Anisotropy");
   const QString AnisotropyBaseName("Anisotropy");
+  
+  const QString Version("1.0");
+  const QString CompatibilityVersion("1.0");
+
+  const QString VendorName("Czech Academy of Sciences, Institute of Physics, Group of Bulk Nanomaterials and Interfaces");
+  const QString URL("http://ams.fzu.cz");
+  const QString Copyright("(C) 2016 Czech Academy of Sciences, v.v.i.");
+  
 }
 #endif
