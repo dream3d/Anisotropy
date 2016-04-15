10ec7aeddf4318810003bc308039a8e8ed85e7fa
Ondrej Sedivy <ondrejsedivy@post.cz>
2016-04-15 14:02:06 +0200

    spacing

--- AnisotropyFilters/AdaptiveAlignmentMutualInformation.cpp
+++ AnisotropyFilters/AdaptiveAlignmentMutualInformation.cpp
@@ -49,6 +49,8 @@
 #include "Anisotropy/AnisotropyConstants.h"
 
 #include "moc_AdaptiveAlignmentMutualInformation.cpp"
+
+
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
