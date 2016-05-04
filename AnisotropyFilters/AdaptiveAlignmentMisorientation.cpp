9ff2bc96f3e0adeba1c9c4fd952b79d8d22aae44
Ondrej Sedivy <ondrejsedivy@post.cz>
2016-05-04 16:09:29 +0200

    set different tolerance and standardize error2

--- AnisotropyFilters/AdaptiveAlignmentMisorientation.cpp
+++ AnisotropyFilters/AdaptiveAlignmentMisorientation.cpp
@@ -388,16 +388,18 @@ void AdaptiveAlignmentMisorientation::find_shifts(std::vector<int64_t>& xshifts,
     std::vector<float> xshiftsest;	// cumulative x-shifts estimated from SEM images
     std::vector<float> yshiftsest;	// cumulative y-shifts estimated from SEM images
 
-    float curerror = 0;
-    float tolerance = 1.0f / static_cast<float>(dims[2]);
+    float curerror = 0.0f;
+    float tolerance = 0.0f;
 
     // evaluate error between current shifts and desired shifts
     if (xneedshifts.size() == 1)      // error is computed as misagreement between slopes
     {
+      tolerance = 1.0f / static_cast<float>(dims[2] - 1);
       curerror = compute_error1(dims[2], 0, xneedshifts[0], yneedshifts[0], newxshift, newyshift, curindex);
     }
     else if (xneedshifts.size() > 1)  // error is computed as misagreement with shifts estimated from SEM images
     {
+      tolerance = 1.0f;
       xshiftsest.resize(dims[2], 0);
       yshiftsest.resize(dims[2], 0);
       for (uint64_t iter = 1; iter < dims[2]; iter++)
@@ -571,6 +573,7 @@ float AdaptiveAlignmentMisorientation::compute_error2(uint64_t iter, uint64_t in
   float error = 0;
   float xdif = 0;
   float ydif = 0;
+  float divide = static_cast<float> (2 * n);
 
   for (uint64_t i = 1; i <= n; i++)
   {
@@ -587,7 +590,7 @@ float AdaptiveAlignmentMisorientation::compute_error2(uint64_t iter, uint64_t in
 
     xdif = static_cast<float>(xshifts)-xshiftsest[i];
     ydif = static_cast<float>(yshifts)-yshiftsest[i];
-    error += (xdif * xdif + ydif * ydif);
+    error += (xdif * xdif + ydif * ydif) / divide;
   }
   return error;
 }
