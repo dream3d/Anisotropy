2bb0fbac6f3d9131ef1ccb8f0a3b8f9e7d09e80f
Michael Jackson <mike.jackson@bluequartz.net>
2018-07-20 10:04:17 -0400

    Update plugin information that is displayed to the user

--- AnisotropyPlugin.cpp
+++ AnisotropyPlugin.cpp
@@ -1,37 +1,37 @@
 /* ============================================================================
-* Copyright (c) 2016 Czech Academy of Sciences, Institute of Physics,
-* Group of Bulk Nanomaterials and Interfaces, http://ams.fzu.cz
-*
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*
-* Redistributions of source code must retain the above copyright notice, this
-* list of conditions and the following disclaimer.
-*
-* Redistributions in binary form must reproduce the above copyright notice, this
-* list of conditions and the following disclaimer in the documentation and/or
-* other materials provided with the distribution.
-*
-* Neither the name of the Czech Academy of Sciences, nor the names of its 
-* contributors may be used to endorse or promote products derived from this 
-* software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The code contained herein was partially funded by the followig grants:
-*    Czech Science Foundation (GA CR), project no. GBP108/12/G043
-*    Czech Ministry of Education, Youth and Sports (MSMT), project no. LM2015087
-*
-* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ * Copyright (c) 2016 Czech Academy of Sciences, Institute of Physics,
+ * Group of Bulk Nanomaterials and Interfaces, http://ams.fzu.cz
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of the Czech Academy of Sciences, nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The code contained herein was partially funded by the followig grants:
+ *    Czech Science Foundation (GA CR), project no. GBP108/12/G043
+ *    Czech Ministry of Education, Youth and Sports (MSMT), project no. LM2015087
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #include "AnisotropyPlugin.h"
 
@@ -44,22 +44,19 @@
 
 #include "Anisotropy/AnisotropyConstants.h"
 
-
-
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-AnisotropyPlugin::AnisotropyPlugin() :
-  m_Version(AnisotropyConstants::Version),
-  m_CompatibilityVersion(AnisotropyConstants::CompatibilityVersion),
-  m_Vendor(AnisotropyConstants::VendorName),
-  m_URL(AnisotropyConstants::URL),
-  m_Location(""),
-  m_Copyright(AnisotropyConstants::Copyright),
-  m_Filters(QList<QString>()),
-  m_DidLoad(false)
+AnisotropyPlugin::AnisotropyPlugin()
+: m_Version(AnisotropyConstants::Version)
+, m_CompatibilityVersion(AnisotropyConstants::CompatibilityVersion)
+, m_Vendor(AnisotropyConstants::VendorName)
+, m_URL(AnisotropyConstants::URL)
+, m_Location("")
+, m_Copyright(AnisotropyConstants::Copyright)
+, m_Filters(QList<QString>())
+, m_DidLoad(false)
 {
-
 }
 
 // -----------------------------------------------------------------------------
@@ -90,7 +87,6 @@ QString AnisotropyPlugin::getPluginBaseName()
 {
   return AnisotropyConstants::AnisotropyBaseName;
 }
-
 
 // -----------------------------------------------------------------------------
 //
@@ -141,9 +137,9 @@ QString AnisotropyPlugin::getDescription()
   QFileInfo licenseFileInfo(licenseFile);
   QString text = "<<--Description was not read-->>";
 
-  if ( licenseFileInfo.exists() )
+  if(licenseFileInfo.exists())
   {
-    if ( licenseFile.open(QIODevice::ReadOnly | QIODevice::Text) )
+    if(licenseFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
       QTextStream in(&licenseFile);
       text = in.readAll();
@@ -169,9 +165,9 @@ QString AnisotropyPlugin::getLicense()
   QFileInfo licenseFileInfo(licenseFile);
   QString text = "<<--License was not read-->>";
 
-  if ( licenseFileInfo.exists() )
+  if(licenseFileInfo.exists())
   {
-    if ( licenseFile.open(QIODevice::ReadOnly | QIODevice::Text) )
+    if(licenseFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
       QTextStream in(&licenseFile);
       text = in.readAll();
@@ -188,18 +184,18 @@ QMap<QString, QString> AnisotropyPlugin::getThirdPartyLicenses()
   QMap<QString, QString> licenseMap;
   QList<QString> fileStrList;
   fileStrList.push_back(":/ThirdParty/HDF5.txt");
-  
+
   fileStrList.push_back(":/ThirdParty/Qt.txt");
   fileStrList.push_back(":/ThirdParty/Qwt.txt");
 
-  for (QList<QString>::iterator iter = fileStrList.begin(); iter != fileStrList.end(); iter++)
+  for(QList<QString>::iterator iter = fileStrList.begin(); iter != fileStrList.end(); iter++)
   {
     QFile file(*iter);
     QFileInfo licenseFileInfo(file);
 
-    if ( licenseFileInfo.exists() )
+    if(licenseFileInfo.exists())
     {
-      if ( file.open(QIODevice::ReadOnly | QIODevice::Text) )
+      if(file.open(QIODevice::ReadOnly | QIODevice::Text))
       {
         QTextStream in(&file);
         licenseMap.insert(licenseFileInfo.baseName(), in.readAll());
@@ -239,7 +235,6 @@ void AnisotropyPlugin::setLocation(QString filePath)
 // -----------------------------------------------------------------------------
 void AnisotropyPlugin::writeSettings(QSettings& prefs)
 {
-
 }
 
 // -----------------------------------------------------------------------------
@@ -247,7 +242,6 @@ void AnisotropyPlugin::writeSettings(QSettings& prefs)
 // -----------------------------------------------------------------------------
 void AnisotropyPlugin::readSettings(QSettings& prefs)
 {
-
 }
 
 // -----------------------------------------------------------------------------
@@ -258,4 +252,3 @@ void AnisotropyPlugin::registerFilterWidgets(FilterWidgetManager* fwm)
 }
 
 #include "AnisotropyFilters/RegisterKnownFilters.cpp"
-
